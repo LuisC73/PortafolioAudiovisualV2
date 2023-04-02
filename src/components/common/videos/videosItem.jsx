fe7bd471a03861cca4b99df73260633cd12e990b
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2023-04-02 08:37:25 -0500

    Se agregan videos

--- src/components/common/videos/videosItem.jsx
+++ src/components/common/videos/videosItem.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import ReactPlayer from "react-player";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
+import images from "../../../helpers/images";
+import videos from "../../../helpers/videos";
 
 function VideosItem({ title, videoURL, posterVideo, description }) {
   return (
     <div className="videosContent__card">
-      <ReactPlayer
-        url={videoURL}
-        controls
-        width="100%"
-        height="420px"
-        className="videosContent__video"
-      />
+      <Video
+        controls={["PlayPause", "Seek", "Time", "Volume", "Fullscreen"]}
+        poster={images[posterVideo]}
+      >
+        <source src={videos[videoURL]} type="video/mp4" />
+        <track
+          label="English"
+          kind="subtitles"
+          srcLang="en"
+          src="http://source.vtt"
+          default
+        />
+      </Video>
       <div className="videosContent__info">
         <h4 className="videosContent__h4">{title}</h4>
         <p className="videosContent__p">{description}</p>
--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,12 +1,8 @@
 const videos = {
-  Promo:
-    "https://drive.google.com/file/d/1C8PzN1oyBlQ_Nd57NJMt1B8b4aeKGOA5/view?usp=share_link",
-  Matizando:
-    "https://drive.google.com/file/d/14Kkb98yaAnhiH4_FULpFJK5wVP8eQan6/view?usp=share_link",
-  Invitacion:
-    "https://drive.google.com/file/d/1u8ywSkq1pbnALGi6cnkgL1Cz6HR7uxyz/view?usp=share_link",
-  Documental:
-    "https://drive.google.com/file/d/1WuARltUNJVzyTmK9d6yK8J9878Av4Nrj/view?usp=share_link",
+  Promo: "../../public/videos/Promo Invitación Fiestas.mp4",
+  Matizando: "../../public/videos/Matizando Ana Sanz.mp4",
+  Invitacion: "../../public/videos/Invitación Documental.mp4",
+  Documental: "../../public/videos/Documental En Conflicto.mp4",
 };
 
 export default videos;
