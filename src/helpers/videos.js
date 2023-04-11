24c04303288bf3a20d1b05c73718b6ce45ca7f52
LuisC73 <luiscastroc11@gmail.com>
2023-04-10 21:05:46 -0500

    Se suben videos

--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,8 +1,14 @@
 const videos = {
-  Promo: "/videos/Promo Invitación Fiestas.mp4",
+  Corto: "/videos/1-Dirección-producción-Corto.mp4",
   Matizando: "/videos/Matizando Ana Sanz.mp4",
   Invitacion: "/videos/Invitación Documental.mp4",
   Documental: "/videos/Documental En Conflicto.mp4",
+  Edicion: "/videos/5-Edición-ReelHondalaestrella.mp4",
+  Avoces: "/videos/6.Ediciónyanimación1.mp4",
+  Animacion: "/videos/7.Animación-Campaña1.mp4",
+  Ranking: "/videos/8.Animación-Campaña.mp4",
+  Mujer: "/videos/9.Producciónyedición.mp4",
+  Produccion: "/videos/10.Producciónyedición.mp4",
 };
 
 export default videos;
