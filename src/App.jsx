9060f42457ed918424b24d9de4b0f8480c17f8f4
LuisC73 <luiscastroc11@gmail.com>
2023-04-16 13:00:18 -0500

    Se agrega loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import MainLayout from "./components/layout/MainLayout/MainLayout";
 import VideosLayout from "./components/layout/VideosLayout/VideosLayour";
+
+import HashLoader from "react-spinners/HashLoader";
 
 const router = createBrowserRouter([
   { path: "/", element: <MainLayout /> },
@@ -10,9 +12,35 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 7000);
+  }, []);
+
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      {loading ? (
+        <div className="loading">
+          <HashLoader
+            color={"#1c6758"}
+            loading={loading}
+            size={60}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            className="loading__item"
+          />
+          <p className="loading__p">
+            Portafolio <br />
+            <span className="loading__span">Ana Manuela Ramirez</span>
+          </p>
+        </div>
+      ) : (
+        <RouterProvider router={router} />
+      )}
     </div>
   );
 }
